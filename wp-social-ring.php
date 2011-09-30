06543194895e04d1d6edbf812ddbdffa1fa5bbea
niccolo_tapparo <niccolo_tapparo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-30 19:08:03 +0000

    changed stable tag
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-social-ring/trunk@445833 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-social-ring.php
+++ wp-social-ring.php
@@ -3,7 +3,7 @@
 Plugin Name: WordPress Social Ring
 Description: Let visitors share posts/pages on Facebook, Twitter and Google+. From admin page you can choose which button display: Facebook Like, Facebook Send, Facebook Share, Google +1 and Twitter.
 Author: Niccol&ograve; Tapparo
-Version: 1.0.1
+Version: 1.0.2
 Author URI: http://wordpress.altervista.org/
 Plugin URI: http://wordpress.altervista.org/temi-realizzati/plugin-wordpress-social-ring/
 */
